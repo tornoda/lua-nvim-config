14038b8888929166050a2c08c14e2069f94c0e61
卢雨龙 <luyulong@luyulongdeMacBook-Pro.local>
2024-08-02 13:01:06 +0800

    feat: diff color2

--- lua/func.lua
+++ lua/func.lua
@@ -9,23 +9,19 @@ M.setColor = function()
     vim.api.nvim_set_hl(0, "DiffText", { bg = "#a6daa9", fg = nil })
     vim.api.nvim_set_hl(0, "DiffChange", { bg = "#def1df", fg = nil })
   else
+    vim.api.nvim_set_hl(0, "DiffChange", { link = "DiffAdd" })
     -- vim.api.nvim_set_hl(0, "DiffAdd", { bg = "#295337", fg = nil })
     -- vim.api.nvim_set_hl(0, "DiffDelete", { bg = "brown", fg = "brown" })
     -- vim.api.nvim_set_hl(0, "DiffText", { bg = "#157d38", fg = nil })
     -- vim.api.nvim_set_hl(0, "DiffChange", { bg = "#295337" })
 
     vim.api.nvim_set_hl(0, "DiffAdd", { bg = "#002716", fg = nil })
-    vim.api.nvim_set_hl(0, "DiffChange", { link = "DiffAdd" })
-    vim.api.nvim_set_hl(0, "DiffDelete", { bg = nil, fg = "#670000" })
     vim.api.nvim_set_hl(0, "DiffText", { bg = "#001F12" })
   end
 
-  -- require("gitsigns").refresh()
+  --  require("gitsigns").refresh()
   -- print(
   --   ("A new %s was opened on tab page %d!")
-  --     :format(view.class:name(), view.tabpage)
-  -- )
-  -- require("base46").load_all_highlights()
 end
 
 return M
