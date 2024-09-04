702cfe40e000da745d3b7ead147ce99f52c14e2e
卢雨龙 <luyulong@luyulongdeMBP.lan>
2024-09-04 23:49:30 +0800

    config add

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -77,10 +77,7 @@ capabilities.textDocument.completion.completionItem = {
   },
 }
 
--- vim.diagnostic.config { virtual_text = false }
-vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
-  virtual_text = false,
-})
+vim.diagnostic.config { virtual_text = true, signs = false }
 
 return {
   {
